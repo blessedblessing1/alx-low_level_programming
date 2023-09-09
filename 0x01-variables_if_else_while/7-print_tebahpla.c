576bbc9c9b187650ff62f63fe048010c13fba88e
blessedblessing1 <oluwablessinga@gmail.com>
2023-09-08 19:09:42 -0700

    Z-A order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Desending order
+ * Return: 0 for smooth execution
+ */
+int main(void)
+{
+	char b;
+
+	b = 'z';
+
+	while
+		(b >= 'a')
+		{
+			putchar(b);
+			b--;
+		}
+	putchar('\n');
+	return (0);
+}
