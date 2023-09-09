026c443f6b2227a4ba79198f5e187e3eb25c5140
blessedblessing1 <oluwablessinga@gmail.com>
2023-09-08 18:17:21 -0700

    counting numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -We are counting
+ * Return: 0 indicates success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+
+	{
+		printf("%d\n", x);
+	}
+	return (0);
+}
+
