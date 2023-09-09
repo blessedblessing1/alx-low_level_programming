a293332f33e535156a3476384f9e4b9982a27541
blessedblessing1 <oluwablessinga@gmail.com>
2023-09-08 19:35:28 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Counting in base 16
+ * Return: 0 always for a successful work
+ */
+int main(void)
+{
+	char z;
+	int y;
+
+	z = 'a';
+	y = '0';
+
+	while
+		(y <= 9)
+		{
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(z <= 'f')
+		{
+			putchar(z);
+		}
+	putchar('\n');
+	return (0);
+}
