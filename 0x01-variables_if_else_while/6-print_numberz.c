932210c4d0fffa15e931f1f9b491f4de8c9df8da
blessedblessing1 <oluwablessinga@gmail.com>
2023-09-08 18:46:51 -0700

    still counting

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -counting from 0-9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	while
+		(x <= 9)
+		{
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
