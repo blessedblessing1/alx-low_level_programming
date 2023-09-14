7ccf2ca8cbe19ff7d87cf48657c736ee3caff7a8
blessedblessing1 <oluwablessinga@gmail.com>
2023-09-13 19:59:43 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Write the absolute value of an integer
+ * @n: the integer to be checked
+ * Return: the absolute value
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
